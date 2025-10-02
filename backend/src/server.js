e22b9104995dd0d9664b805f40054a4b89ee72f5
Shashank2125 <138694950+Shashank2125@users.noreply.github.com>
2025-10-02 19:22:18 +0530

    prepared for the deployement

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,19 @@ import express from "express";
 import  notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import path from "path";
 import ratelimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
 dotenv.config();
 const app=express();
 const PORT=process.env.PORT || 5001;
-
+const __dirname=path.resolve()
+//if not in production add cors config because of two domain
+if(process.env.NODE_ENV!=="production"){
 app.use(cors({
   origin:"http://localhost:5173",
 }));
+}
 app.use(express.json());//this middleware will parse JSON bodies: req.body
 //middleware
 app.use(ratelimiter);
@@ -26,6 +30,15 @@ app.use(ratelimiter);
 //})
 
 app.use("/api/notes",notesRoutes);
+//if in produciton add this to make two doamin into single domain for production
+if(process.env.NODE_ENV==="production"){
+//make the dist folder in frontend static
+app.use(express.static(path.join(__dirname,"../frontend/dist")))
+//if the 
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+});
+}
 
 connectDB().then(()=>{
 //we take the endpoints from notes routes and prefix them with /api/notes
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
+//in production there is no loal host to make our domain dynamic we do this
+const Base_URL=import.meta.env.MODE=="development"?"http://localhost:5001/api" :"/api"
 const api=axios.create({
-    baseURL:"http://localhost:5001/api",
+    baseURL: Base_URL,
     //we are not adding /notes because let say we have to update 
     //let say we hvae /user then we cannot use this 
 });
+
 export default api;
